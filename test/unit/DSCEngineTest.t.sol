97dbbf1d8bb49b5812c229bd674cf911f9dfb683
Vladislav Zavtur <ychoeth@proton.me>
2024-03-09 15:49:40 +0200

    some tests

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "@/script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "@/src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "@/src/DSCEngine.sol";
+import {HelperConfig} from "@/script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DecentralizedStableCoin dsc;
+    DSCEngine engine;
+    HelperConfig config;
+
+    address ethUsdPriceFeed;
+    address weth;
+    address wbtcUsdPriceFeed;
+    address wbtc;
+
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, engine, config) = deployer.run();
+        (ethUsdPriceFeed, wbtcUsdPriceFeed, weth, wbtc, ) = config.activeNetworkConfig();
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }
+
+    /////////////////
+    // Price Tests //
+    /////////////////
+
+    function testGetUsdValue() public {
+        uint256 ethAmount = 15e18;
+        uint256 expectedEthUsdValue = 60000e18;
+        uint256 actualEthUsd = engine.getUsdValue(weth, ethAmount);
+        assertEq(actualEthUsd, expectedEthUsdValue);
+
+        uint256 btcAmount = 2e8;
+        uint256 expectedBtcUsdValue = 140000e8;
+        uint256 actualBtcUsd = engine.getUsdValue(wbtc, btcAmount);
+        assertEq(actualBtcUsd, expectedBtcUsdValue);
+    }
+
+    //////////////////////
+    // Collateral Tests //
+    //////////////////////
+
+    function testRevertsIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(engine), AMOUNT_COLLATERAL);
+        vm.expectRevert(DSCEngine.DSCEngine__NeedMoreThanZero.selector);
+        engine.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+
+}
